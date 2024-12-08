775e83d7749d2135f92b16af89936ff8a96495c7
goutham-shankar <gouthamsankarv@gmail.com>
2024-12-08 21:27:36 +0530

    block

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 // src/App.js
 import React from 'react';
 import DataTable from './Sheet';
-import Footer from './Footer';
 
 
 
 function App() {
   return (
     <div className="App">
-  
+
       <DataTable />
     </div>
   );
